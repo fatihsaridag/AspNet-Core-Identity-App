67189b08aa2f4cc530c780333b64895e5ce36ff4
Fatih Saridag <fatihsaridag26@gmail.com>
2022-11-02 14:56:15 +0300

    Rol oluşturma işlemi başarıyla gerçekleşti (36)

---
+++ IdentityApp/ViewModels/RoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityApp.ViewModels
+{
+    public class RoleViewModel
+    {
+        [Display(Name="Role")]
+        [Required(ErrorMessage ="Role ismi gereklidir.")]
+        public string Name { get; set; }
+        public string Id { get; set; }          //Id'yi arka tarafta tutucaz güncelleme işleminde kullanıcaz.
+    }
+}
